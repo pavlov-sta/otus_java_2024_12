eaa5a632c5d69df1b5c2bb43c9c989b536dfd050
Pavlov Stanislav <pavlov@metasharks.ru>
2025-01-06 22:38:26 +0300

    feat: hw01: Create hw01-gradle project

---
+++ hw01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -0,0 +1,11 @@
+package ru.otus;
+
+import com.google.common.base.Strings;
+
+public class HelloOtus {
+
+    public static void main(String[] args) {
+        String result = Strings.repeat("Hello, Otus! ", 1);
+        System.out.println("Result: " + result);
+    }
+}
