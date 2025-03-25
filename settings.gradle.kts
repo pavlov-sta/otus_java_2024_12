3b6fb4490dae52c968d68da17a9b45a1b6cb2832
stvort <stvort@mail.ru>
2025-03-25 20:54:40 +0400

    L21-jpql added
    (cherry picked from commit 87b1421a1188e326f737ee6a57e91725c4413ad1)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,9 @@ include("L16-io:homework")
 include("L18-jdbc:demo")
 include("L18-jdbc:homework")
 
+include("L21-jpql:class-demo")
+include("L21-jpql:homework-template")
+
 pluginManagement {
     val jgitver: String by settings
     val dependencyManagement: String by settings
