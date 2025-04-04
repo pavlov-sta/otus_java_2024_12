7abb13cac33a81b52c9a0ffb004d3639ceb5f25d
Pavlov Stanislav <pavlov-sta@mail.ru>
2025-04-04 11:48:52 +0300

    feat:L22: realize MyCache

--- L22-cache/src/main/java/ru/otus/cachehw/MyCache.java
+++ L22-cache/src/main/java/ru/otus/cachehw/MyCache.java
@@ -1,30 +1,48 @@
 package ru.otus.cachehw;
 
+import java.util.List;
+import java.util.Map;
+import java.util.WeakHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class MyCache<K, V> implements HwCache<K, V> {
+
     // Надо реализовать эти методы
+    private final Map<K, V> cache = new WeakHashMap<>();
+    private final List<HwListener<K, V>> listeners = new CopyOnWriteArrayList<>();
 
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        cache.put(key, value);
+        notificationListeners(key, value, "put");
     }
 
     @Override
     public void remove(K key) {
-        throw new UnsupportedOperationException();
+        var value = cache.remove(key);
+        notificationListeners(key, value, "remove");
     }
 
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        var value = cache.get(key);
+        notificationListeners(key, value, "get");
+        return value;
     }
 
     @Override
     public void addListener(HwListener<K, V> listener) {
-        throw new UnsupportedOperationException();
+        listeners.add(listener);
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
-        throw new UnsupportedOperationException();
+        listeners.remove(listener);
+    }
+
+    private void notificationListeners(K key, V value, String action) {
+        for (HwListener<K, V> listener : listeners) {
+            listener.notify(key, value, action);
+        }
     }
 }
